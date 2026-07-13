7109064806dfe56b6947ffe654d1dd560c9ffc30
LeThanhDat111 <ltd2006111@gmail.com>
2026-07-13 13:14:35 +0700

    Optimize code by destructuring and spread operator

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 import './App.css';
 import logo from './assets/img/my_logo.png';
-import pic1 from './assets/img/images.jpg';
-import pic2 from './assets/img/React-la-gi-1.jpg';
-import pic3 from './assets/img/React-la-gi-6518027.jpg';
-import pic4 from './assets/img/Reactjs.webp';
+import { myData } from './data.js';
 
 function Header() {
 	return (
@@ -24,10 +21,10 @@ function Main() {
 				<section>
 					<h2>Khái niệm chính trong React</h2>
 					<ul>
-						<Main_Information img={pic1} title="Components" desc="Khối xây dựng giao diện cơ bản - Kết hợp nhiều thành phần để tạo nên ứng dụng" />
-						<Main_Information img={pic2} title="Components" desc="Khối xây dựng giao diện cơ bản - Kết hợp nhiều thành phần để tạo nên ứng dụng" />
-						<Main_Information img={pic3} title="Components" desc="Khối xây dựng giao diện cơ bản - Kết hợp nhiều thành phần để tạo nên ứng dụng" />
-						<Main_Information img={pic4} title="Components" desc="Khối xây dựng giao diện cơ bản - Kết hợp nhiều thành phần để tạo nên ứng dụng" />
+						<Main_Information img={myData[0].img} title={myData[0].title} desc={myData[0].desc} />
+						<Main_Information {...myData[1]} />
+						<Main_Information {...myData[2]} />
+						<Main_Information {...myData[3]} />
 					</ul>
 				</section>
 			</main>
@@ -42,7 +39,6 @@ function Main_Information(props) {
 				<figure>
 					<img src={props.img} alt={props.title} className="img_main img_hover" />
 				</figure>
-				<h3>{props.title}</h3>
 				<p>{props.desc}</p>
 			</li>
 		</>
---
+++ src/data.js
@@ -0,0 +1,28 @@
+import pic1 from "./assets/img/images.jpg";
+import pic2 from "./assets/img/React-la-gi-1.jpg";
+import pic3 from "./assets/img/React-la-gi-6518027.jpg";
+import pic4 from "./assets/img/Reactjs.webp";
+
+export const myData = [
+    {
+        img: pic1,
+        title: 'Components',
+        desc: 'Khối xây dựng giao diện cơ bản - kết hợp nhiều thành phần để tạo nên ứng dụng',
+
+    },
+    {
+        img: pic2,
+        title: 'JSX',
+        desc: 'Kết hợp HTML và JSX để tạo giao diện rộng và mạnh mẽ',
+    },
+    {
+        img: pic3,
+        title: 'Props',
+        desc: 'Truyền dữ liệu vào thành phần để làm nó linh hoạt và tái sử dụng',
+    },
+    {
+        img: pic4,
+        title: 'State',
+        desc: 'Dữ liệu được React quản lý, khi thay đổi sẽ tự động làm mới giao diện',
+    },
+]
