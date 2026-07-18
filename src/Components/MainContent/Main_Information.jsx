94f9706ac5e141c78e2773d49c743a599c0061c8
LeThanhDat111 <ltd2006111@gmail.com>
2026-07-18 15:30:53 +0700

    dialog_warming

--- src/Components/MainContent/Main_Information.jsx
+++ src/Components/MainContent/Main_Information.jsx
@@ -1,6 +1,8 @@
+import { useState } from 'react';
 import { myData, EXAMPLE } from '../../data.js';
 import './Main_Information.css';
 import TabButton from './Tab_Button.jsx';
+import performace from '../../assets/img/energy.png';
 
 function Main_Information(props) {
 	return (
@@ -16,6 +18,23 @@ function Main_Information(props) {
 }
 
 function Main({ onSelect, prinHello, hello, slTopic }) {
+	const [isActivated, setIsActivated] = useState(false);
+	const [isAlertVisible, setIsAlertVisible] = useState(false);
+
+	function handleAlertVisible() {
+		setIsAlertVisible(true);
+	}
+
+	function handleConfirm() {
+		setIsAlertVisible(false);
+		setIsActivated(true);
+	}
+
+	function handleCancel() {
+		setIsAlertVisible(false);
+		setIsActivated(false);
+	}
+
 	return (
 		<>
 			<main>
@@ -44,7 +63,7 @@ function Main({ onSelect, prinHello, hello, slTopic }) {
 					</TabButton>
 				</ul>
 				{!slTopic ? (
-					<p>Vui lòng chọn tab cần tìm hiểu về React!</p>
+					<p className="txt_info-btn">Vui lòng chọn tab cần tìm hiểu về React!</p>
 				) : (
 					<div className="bx_info-btn">
 						<h3>{EXAMPLE[slTopic].title}</h3>
@@ -59,6 +78,28 @@ function Main({ onSelect, prinHello, hello, slTopic }) {
 					<button className="btn_State" onClick={prinHello}>
 						Cập nhật lời chào
 					</button>
+				</div>
+				<div className="active-dialog">
+					{!isActivated && !isAlertVisible && (
+						<button className="btn_active" onClick={handleAlertVisible}>
+							Active
+						</button>
+					)}
+					{isAlertVisible && (
+						<div className="diaglog">
+							<h3>Warming!</h3>
+							<p>Are you sure you want to active this mode?</p>
+							<div className="bx_btn-chose">
+								<button className="btn_Confirm" onClick={handleConfirm}>
+									Confirm
+								</button>
+								<button className="btn_Cancel" onClick={handleCancel}>
+									Cancel
+								</button>
+							</div>
+						</div>
+					)}
+					{isActivated && <p className='mode_activating'>Mode activating <img src={performace} alt="" className='img_performance'/></p>}
 				</div>
 			</main>
 		</>
