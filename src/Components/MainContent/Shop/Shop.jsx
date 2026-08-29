38624652e1f479f7a9e8bf89d67acfa9f86adea1
LeThanhDat111 <ltd2006111@gmail.com>
2026-08-29 22:28:22 +0700

    Use useEffect

--- src/Components/MainContent/Shop/Shop.jsx
+++ src/Components/MainContent/Shop/Shop.jsx
@@ -1,12 +1,20 @@
 import { useState } from 'react';
 import ListProduct from './ListProduct';
 import CartContext from './CartContext';
+import { useEffect } from 'react';
+
 export default function Shop() {
 	const [amountItem, setAmountItem] = useState(0);
 	function handleAmountItem() {
 		setAmountItem((amountItem) => amountItem + 1);
 	}
 	console.log(amountItem);
+	useEffect(() => {
+		fetch('https://fakestoreapi.com/products')
+			.then((response) => response.json())
+			.then((data) => console.log(data));
+		console.log('Đã render lại');
+	}, []);
 	return (
 		<CartContext.Provider value={{ handleAmountItem, amountItem }}>
 			<div>
